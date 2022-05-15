9b725f2b16c8a569012704af663c6b58625f5298
Lukas Kuppers <lkuppers11@gmail.com>
2022-05-15 16:18:19 -0400

    Add delimmited text file reader

--- UnityProject/Assets/Scripts/Gameplay/Progression/StoryNotesManager.cs
+++ UnityProject/Assets/Scripts/Gameplay/Progression/StoryNotesManager.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class StoryNotesManager : MonoBehaviour
 {
     private List<int> discoveredNotes;
+    private string[][] storyNoteData;
 
     private void Start()
     {
         discoveredNotes = new List<int>();
+
+        DelimmitedTextLoader noteLoader = new DelimmitedTextLoader("StoryNotes", '\t');
+        storyNoteData = noteLoader.GetDataNoHeader();
+
+        foreach (string[] row in storyNoteData)
+        {
+            foreach (string cell in row)
+            {
+                Debug.Log(cell);
+            }
+        }
     }
 
     public List<int> GetDiscoveredNotes()
---
+++ UnityProject/Assets/Scripts/Serialization/DelimmitedTextLoader.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelimmitedTextLoader
+{
+    private readonly TextAsset textAsset;
+
+    private char delimmiterChar;
+
+    public DelimmitedTextLoader(string fileName, char delimmiter)
+    {
+        textAsset = Resources.Load<TextAsset>(fileName);
+        delimmiterChar = delimmiter;
+    }
+
+    public string GetRawData()
+    {
+        return textAsset.text;
+    }
+
+    public string[][] GetData()
+    {
+        string[] rowsRaw = textAsset.text.Split(new char[] { '\n' });
+        List<string[]> parsedDataLst = new List<string[]>();
+
+        for (int i = 0; i < rowsRaw.Length; i++)
+        {
+            if (rowsRaw[i] != "")
+            {
+                string[] parsedRow = rowsRaw[i].Split(new char[] { delimmiterChar });
+                parsedDataLst.Add(parsedRow);
+            }
+        }
+
+        return parsedDataLst.ToArray();
+    }
+
+    public string[][] GetDataNoHeader()
+    {
+        string[][] allData = GetData();
+
+        List<string[]> allRowsLst = new List<string[]>(allData);
+        allRowsLst.RemoveAt(0);
+        return allRowsLst.ToArray();
+    }
+}
