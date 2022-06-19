714561ee7c503dbe8f96c99bfcdb4965d4e43239
Lukas Kuppers <lkuppers11@gmail.com>
2022-06-19 19:34:34 -0400

    Add Audio Manager

---
+++ UnityProject/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Events;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    private Sound[] sounds;
+
+    private Dictionary<string, AudioSource> audioSources;
+
+    private static AudioManager singletonInstance;
+
+    public static AudioManager GetInstance()
+    {
+        if (singletonInstance == null)
+            singletonInstance = FindObjectOfType<AudioManager>();
+
+        return singletonInstance;
+    }
+
+    private void Awake()
+    {
+        audioSources = new Dictionary<string, AudioSource>();
+
+        foreach (Sound sound in sounds)
+        {
+            AudioSource source = gameObject.AddComponent<AudioSource>();
+
+            source.clip = sound.clip;
+            source.volume = sound.volume;
+            source.pitch = sound.pitch;
+            source.loop = sound.loop;
+
+            audioSources.Add(sound.name, source);
+        }
+    }
+
+    public void Play(string soundName)
+    {
+        FetchSource(soundName, (source) =>
+        {
+            source.Play();
+        });
+    }
+
+    public void Pause(string soundName)
+    {
+        FetchSource(soundName, (source) =>
+        {
+            source.Pause();
+        });
+    }
+
+    private AudioSource FetchSource(string soundName, UnityAction<AudioSource> callback)
+    {
+        if (audioSources.ContainsKey(soundName))
+        {
+            AudioSource source = audioSources[soundName];
+            callback.Invoke(source);
+            return source;
+        }
+        else
+            Debug.LogWarning($"AudioManager:Play: Sound {soundName} does not exist");
+        return null;
+    }
+}
