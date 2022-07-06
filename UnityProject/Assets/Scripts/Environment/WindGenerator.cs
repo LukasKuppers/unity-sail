7e95af4a78e0455dc2561c2b3b5846b536136919
Lukas Kuppers <lkuppers11@gmail.com>
2022-07-05 23:46:05 -0400

    Dynamically change wind direction

--- UnityProject/Assets/Scripts/Environment/WindGenerator.cs
+++ UnityProject/Assets/Scripts/Environment/WindGenerator.cs
@@ -4,10 +4,41 @@ using UnityEngine;
 
 public class WindGenerator : MonoBehaviour
 {
-    private float direction = 0;
+    private static readonly float COROUTINE_WAIT_TIME = 1.0f;
+
+    [SerializeField]
+    private float windChangeSpeed = 1f;
+
+    private float direction;
+    private float targetDirection;
+
+    private void Start()
+    {
+        direction = Random.Range(0f, 360f);
+        targetDirection = Random.Range(0f, 360f);
+
+        StartCoroutine(WindMoveCoroutine());
+    }
 
     public float GetWindDirection()
     {
         return direction;
+    }
+
+    private IEnumerator WindMoveCoroutine()
+    {
+        while (true)
+        {
+            if (Mathf.Abs(targetDirection - direction) < windChangeSpeed)
+            {
+                targetDirection = Random.Range(0f, 360f);
+            }
+            else
+            {
+                direction += Mathf.Sign(targetDirection - direction) * windChangeSpeed;
+            }
+
+            yield return new WaitForSeconds(COROUTINE_WAIT_TIME);
+        }
     }
 }
