086e537ce3d9c3dd965942b35e730e4d572b6368
Lukas Kuppers <lkuppers11@gmail.com>
2022-08-08 22:09:52 -0400

    Display island name banners on visit

---
+++ UnityProject/Assets/Scripts/UI/SceneInteraction/IslandVisitNotifier.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Globalization;
+
+public class IslandVisitNotifier : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject hudParent;
+    [SerializeField]
+    private GameObject islandVisitManager;
+
+    [SerializeField]
+    private GameObject islandVisitBannerPrefab;
+
+    private IslandVisitManager visitManager;
+
+    private void Start()
+    {
+        visitManager = islandVisitManager.GetComponent<IslandVisitManager>();
+
+        visitManager.AddGeneralVisitListener(DisplayVisitBanner);
+    }
+
+    private void DisplayVisitBanner(Islands island)
+    {
+        GameObject banner = Instantiate(islandVisitBannerPrefab, hudParent.transform);
+
+        TextMeshProUGUI bannerText = banner.GetComponentInChildren<TextMeshProUGUI>();
+
+        TextInfo textinfo = new CultureInfo("en-US", false).TextInfo;
+        string islandName = island.ToString();
+        islandName = islandName.Replace('_', ' ');
+        islandName = textinfo.ToTitleCase(islandName.ToLower());
+        bannerText.text = islandName;
+    }
+}
