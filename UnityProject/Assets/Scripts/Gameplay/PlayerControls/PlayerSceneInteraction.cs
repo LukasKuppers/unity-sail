129d26d2f8c2a2a0fdf6e352cf7a78928b331dce
Lukas Kuppers <lkuppers11@gmail.com>
2022-08-12 22:12:33 -0400

    Add interaction change listener

--- UnityProject/Assets/Scripts/Gameplay/PlayerControls/PlayerSceneInteraction.cs
+++ UnityProject/Assets/Scripts/Gameplay/PlayerControls/PlayerSceneInteraction.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public static class PlayerSceneInteraction
 {
     private static bool interactionEnabled = true;
 
     private static List<string> lockKeys = new List<string>();
+    private static UnityEvent<bool> changeEvent;
 
     public static bool InteractionEnabled()
     {
@@ -17,6 +19,9 @@ public static class PlayerSceneInteraction
     {
         if (!lockKeys.Contains(key))
         {
+            if (changeEvent != null && interactionEnabled)
+                changeEvent.Invoke(false);
+
             interactionEnabled = false;
             lockKeys.Add(key);
         }
@@ -30,7 +35,18 @@ public static class PlayerSceneInteraction
         }
         if (lockKeys.Count == 0)
         {
+            if (changeEvent != null && !interactionEnabled)
+                changeEvent.Invoke(true);
+
             interactionEnabled = true;
         }
+    }
+
+    public static void AddInteractionChangeListener(UnityAction<bool> call)
+    {
+        if (changeEvent == null)
+            changeEvent = new UnityEvent<bool>();
+
+        changeEvent.AddListener(call);
     }
 }
