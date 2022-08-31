b7b3fb75e98750e3db8f4325680430125c11b643
Lukas Kuppers <lkuppers11@gmail.com>
2022-08-31 17:20:48 -0400

    Simplify player attack mode classa

--- UnityProject/Assets/Scripts/Gameplay/PlayerControls/PlayerAttackMode.cs
+++ UnityProject/Assets/Scripts/Gameplay/PlayerControls/PlayerAttackMode.cs
@@ -4,33 +4,22 @@ using UnityEngine;
 
 public static class PlayerAttackMode
 {
-    private static bool attackEnabled = true;
-
     private static List<string> lockKeys = new List<string>();
 
     public static bool AttackEnabled()
     {
-        return attackEnabled;
+        return lockKeys.Count == 0;
     }
 
     public static void DisableAttack(string key)
     {
         if (!lockKeys.Contains(key))
-        {
-            attackEnabled = false;
             lockKeys.Add(key);
-        }
     }
 
     public static void EnableAttack(string key)
     {
         if (lockKeys.Contains(key))
-        {
             lockKeys.Remove(key);
-        }
-        if (lockKeys.Count == 0)
-        {
-            attackEnabled = true;
-        }
     }
 }
