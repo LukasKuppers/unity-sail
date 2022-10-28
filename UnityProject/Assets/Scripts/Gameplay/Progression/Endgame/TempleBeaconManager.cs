ebe340690b7fe8c6922b2558139cb6d2e96dbce7
Lukas Kuppers <lkuppers11@gmail.com>
2022-10-27 22:31:28 -0400

    Add temple beacon manager

---
+++ UnityProject/Assets/Scripts/Gameplay/Progression/Endgame/TempleBeaconManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempleBeaconManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject beaconPrefab;
+    [SerializeField]
+    private GameObject destroyedBeaconPrefab;
+    [SerializeField]
+    private GameObject beacon;
+
+    private GameObject destroyedBeacon;
+
+    private void Start()
+    {
+        // beacon not destroyed by default
+        if (beacon == null)
+            RespawnBeacon();
+        else
+            beacon.GetComponent<IDestructable>().AddDestructionListener(OnBeaconDestroyed);
+
+    }
+
+    public void RespawnBeacon()
+    {
+        if (beacon == null)
+        {
+            if (destroyedBeacon != null)
+                Destroy(destroyedBeacon);
+
+            beacon = Instantiate(beaconPrefab, transform.position, transform.rotation, gameObject.transform);
+            beacon.GetComponent<IDestructable>().AddDestructionListener(OnBeaconDestroyed);
+        }
+    }
+
+    public void SetBeaconDestroyed()
+    {
+        if (destroyedBeacon == null)
+        {
+            if (beacon != null)
+                beacon.GetComponent<IDestructable>().Destroy();
+            else
+                destroyedBeacon = Instantiate(destroyedBeaconPrefab, transform.position, transform.rotation, gameObject.transform);
+        }
+    }
+
+    private void OnBeaconDestroyed(GameObject newDestroyedObject)
+    {
+        beacon = null;
+        destroyedBeacon = newDestroyedObject;
+    }
+}
