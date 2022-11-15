23bde79dc722f7e5b6cdf756486baec577586614
Lukas Kuppers <lkuppers11@gmail.com>
2022-11-14 21:01:35 -0500

    Remove ArenaShipManager

--- UnityProject/Assets/Scripts/Arena/ArenaLifecycleManager.cs
+++ UnityProject/Assets/Scripts/Arena/ArenaLifecycleManager.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class ArenaLifecycleManager : MonoBehaviour
 {
     [SerializeField]
+    private GameObject shipPrefabManagerObject;
+    [SerializeField]
     private GameObject shipSafetyManagerObject;
 
+    private ShipPrefabManager shipPrefabManager;
     private ShipSafetyManager safetyManager;
 
     private void Start()
     {
+        shipPrefabManager = shipPrefabManagerObject.GetComponent<ShipPrefabManager>();
         safetyManager = shipSafetyManagerObject.GetComponent<ShipSafetyManager>();
 
+        shipPrefabManager.SpawnShip(0);
         safetyManager.SetShipSafety(false);
     }
 }
